021dfdb0b5899bc3c5489fcfb92acb82caab86b7
Maria Eduarda de Oliveira Furini <129913960+MaduFurini@users.noreply.github.com>
2024-03-19 20:16:54 -0300

    Atividade de revisão

---
+++ Revisao/Cliente.java
@@ -0,0 +1,45 @@
+package Revisao;
+
+public class Cliente {
+    
+    private String nome;
+    private int idade;
+    private String email;
+
+
+    public void setNome(String name){
+        this.nome = name;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setIdade(int age){
+        this.idade = age;
+    }
+
+    public int getIdade(){
+        return this.idade;
+    }
+
+    public void setEmail(String mail){
+        this.email = mail;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+
+    public Cliente(String nome, int idade, String email){
+        this.nome = nome;
+        this.idade = idade;
+        this.email = email;
+    }
+
+    public Cliente(){
+        this.nome = "Maria";
+        this.idade = 19;
+        this.email = "madufurini@gmail.com";
+    }
+}
